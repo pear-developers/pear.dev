6b3e4639f9922c162af5a666ddf4fd82cf8eb96a
ThibaudMZN <thibaud.marazano@gmail.com>
2022-03-01 08:18:05 +0100

    Creating room on enter

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,111 +1,129 @@
 <script>
-    let room = "";
+	let room = '';
+	let createRoom;
 </script>
 
 <main>
-    <header>
-        <figure>
-            <img src="/type-logo.svg" alt="Pear logo with the text pair.dev">
-        </figure>
-        <h1>
-            <span>Start</span>
-            <span class="words">
-                <span>pair</span>
-                <span>mob</span>
-            </span>
-            <span>programming!</span>
-        </h1>
-        <div class="input-group">
-            <input type="text" placeholder="room" bind:value={room}><a href="/rooms/{room}">create</a>
-        </div>
-    </header>
+	<header>
+		<figure>
+			<img src="/type-logo.svg" alt="Pear logo with the text pair.dev" />
+		</figure>
+		<h1>
+			<span>Start</span>
+			<span class="words">
+				<span>pair</span>
+				<span>mob</span>
+			</span>
+			<span>programming!</span>
+		</h1>
+		<div class="input-group">
+			<input
+				type="text"
+				placeholder="room"
+				bind:value={room}
+				on:change={() => {
+					createRoom.click();
+				}}
+			/><a bind:this={createRoom} href="/rooms/{room}">create</a>
+		</div>
+	</header>
 </main>
 
 <style>
-    :root {
-        background-color: #F6FCFF;
-    }
+	:root {
+		background-color: #f6fcff;
+	}
 
-    header {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        margin-top: 4rem;
-        width: 100vw;
-    }
+	header {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		margin-top: 4rem;
+		width: 100vw;
+	}
 
-    img {
-        width: 20vw;
-    }
+	img {
+		width: 20vw;
+	}
 
-    input {
+	input {
 		font-family: 'Poppins', sans-serif;
-        font-weight: 500;
-        font-size: 24px;
-        background-color: #ECFDE6;
-        padding: 1rem;
-        border-radius: 10px;
-        border: 1px solid #83CC69;
-        width: 15vw;
-    }
-    input:focus {
-        outline: none;
-    }
-    input::placeholder {
-        color: #9DCF8A;
-    }
+		font-weight: 500;
+		font-size: 24px;
+		background-color: #ecfde6;
+		padding: 1rem;
+		border-radius: 10px;
+		border: 1px solid #83cc69;
+		width: 15vw;
+	}
+	input:focus {
+		outline: none;
+	}
+	input::placeholder {
+		color: #9dcf8a;
+	}
 
-    a {
-        display: inline-block;
+	a {
+		display: inline-block;
 		font-family: 'Poppins', sans-serif;
-        font-weight: 500;
-        font-size: 24px;
-        text-decoration: none;
-        background-color: #83CC69;
-        color: #597C4C;
-        padding: 1rem;
-        border-radius: 10px;
-        border: 1px solid #83CC69;
-        cursor: pointer;
-    }
+		font-weight: 500;
+		font-size: 24px;
+		text-decoration: none;
+		background-color: #83cc69;
+		color: #597c4c;
+		padding: 1rem;
+		border-radius: 10px;
+		border: 1px solid #83cc69;
+		cursor: pointer;
+	}
 
-    .input-group {
-        margin-top: 10px;
-        padding: 0.3rem;
-        border-radius: 15px;
-        border: 2px solid #83CC69;
-    }
-    .input-group :nth-child(1) {
-        border-top-right-radius: 0 !important;
-        border-bottom-right-radius: 0 !important;
-    }
-    .input-group :nth-child(2) {
-        border-top-left-radius: 0 !important;
-        border-bottom-left-radius: 0 !important;
-    }
+	.input-group {
+		margin-top: 10px;
+		padding: 0.3rem;
+		border-radius: 15px;
+		border: 2px solid #83cc69;
+	}
+	.input-group :nth-child(1) {
+		border-top-right-radius: 0 !important;
+		border-bottom-right-radius: 0 !important;
+	}
+	.input-group :nth-child(2) {
+		border-top-left-radius: 0 !important;
+		border-bottom-left-radius: 0 !important;
+	}
 
-    h1 {
+	h1 {
 		font-family: 'Poppins', sans-serif;
-        font-weight: 500;
-        color: #3F3838;
-        height: 48px;
-        width: auto;
-        overflow: hidden;
-    }
+		font-weight: 500;
+		color: #3f3838;
+		height: 48px;
+		width: auto;
+		overflow: hidden;
+	}
 
-    .words {
-        display: inline-flex;
-        flex-direction: column;
-        animation: flip-words 6s infinite;
-        text-align: center;
-        font-weight: 700;
-    }
+	.words {
+		display: inline-flex;
+		flex-direction: column;
+		animation: flip-words 6s infinite;
+		text-align: center;
+		font-weight: 700;
+	}
 
-    @keyframes flip-words {
-        0% {transform: translateY(0%);}
-        25% {transform: translateY(-50%);}
-        50% {transform: translateY(-50%);}
-        75% {transform: translateY(0%);}
-        100% {transform: translateY(0%);}
-    }
+	@keyframes flip-words {
+		0% {
+			transform: translateY(0%);
+		}
+		25% {
+			transform: translateY(-50%);
+		}
+		50% {
+			transform: translateY(-50%);
+		}
+		75% {
+			transform: translateY(0%);
+		}
+		100% {
+			transform: translateY(0%);
+		}
+	}
 </style>
