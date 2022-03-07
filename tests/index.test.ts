9603f8211d11613ffdc5509a53179588b0d7c436
ThibaudMZN <thibaud.marazano@gmail.com>
2022-03-07 16:52:24 +0100

    🔧 Adding typescript

---
+++ tests/index.test.ts
@@ -0,0 +1,59 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import '@testing-library/jest-dom';
+import { jest } from '@jest/globals';
+import { fireEvent, render } from '@testing-library/svelte';
+import Index from '../src/routes/index.svelte';
+
+describe('Index', () => {
+	test('shows content when rendered', () => {
+		const { getByText, getByAltText, getByRole } = render(Index);
+
+		expect(getByText('PEAR.DEV')).toBeInTheDocument();
+		expect(getByText('Timing is everything!')).toBeInTheDocument();
+
+		const image = getByAltText('Ticking clock shaped like a pear.');
+		expect(image).toBeInTheDocument();
+		expect(image).toHaveAttribute('src', '/pear-clock.gif');
+
+		const input = getByRole('textbox');
+		expect(input).toBeInTheDocument();
+		expect(input).toHaveAttribute('placeholder', 'Enter room name');
+
+		const button = getByText('GO');
+		expect(button).toBeInTheDocument();
+	});
+
+	test('sets button link to room name', async () => {
+		const { getByText, getByRole } = render(Index);
+		const input = getByRole('textbox');
+		const button = getByText('GO');
+
+		await fireEvent.input(input, { target: { value: 'test-room-name' } });
+
+		expect(button).toHaveAttribute('href', '/rooms/test-room-name');
+		expect(button).toHaveAttribute('disabled', 'false');
+	});
+
+	test('clicks on button when input change', async () => {
+		const { getByText, getByRole } = render(Index);
+		const input = getByRole('textbox');
+		const button = getByText('GO');
+
+		const onClick = jest.fn();
+		button.addEventListener('click', onClick);
+
+		await fireEvent.change(input, { target: { value: 'test-room-name' } });
+
+		expect(onClick).toHaveBeenCalled;
+	});
+
+	test('disables button if no room name', () => {
+		const { getByText } = render(Index);
+		const button = getByText('GO');
+
+		expect(button).toHaveAttribute('disabled', 'true');
+	});
+});
